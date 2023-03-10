567683729fb56f7f05f7ce7aff1373dd0e47024b
anheejeong <eyrt6973@navr.com>
2023-03-11 01:08:14 +0900

    파일 추가

---
+++ hello-spring/src/main/java/HJ/hellospring/controller/HelloController.java
@@ -0,0 +1,49 @@
+package HJ.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello") // url에서 /hello를 받아왔다 !
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!"); // data:hello!!
+        return "hello"; // resources -> templates -> hello.html 찾으란 소리
+    }
+
+    @GetMapping("hello-mvc")
+    public String mvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // http body 부분에 이 데이타를 직접 넣어주겠다는 뜻
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
